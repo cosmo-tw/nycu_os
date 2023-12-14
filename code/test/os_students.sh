a6b86fd93bbd62c1e41595e4b5b002117200d559
cosmo <311512079@nycu.edu.tw>
2023-12-14 20:18:05 +0800

    test

--- code/test/os_students.sh
+++ code/test/os_students.sh
@@ -1,4 +1,4 @@
 make distclean
 make 
-timeout 1 ../build.linux/nachos -ep consoleIO_test1 70 -ep consoleIO_test2 80 -ep consoleIO_test3 50
+timeout 1 ../build.linux/nachos -ep consoleIO_test1 70 -ep consoleIO_test3 80 -ep consoleIO_test2 50
 echo "done"
