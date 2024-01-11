ad5707c834c8c78aa307fa89a4a64e589e37413b
311512023 <xup6rup4cl62333@gmail.com>
2024-01-11 11:11:06 +0800

    240111-1111

--- code/filesys/filesys.h
+++ code/filesys/filesys.h
@@ -59,6 +59,45 @@ class FileSystem {
 	  return new OpenFile(fileDescriptor);
       }
 
+	  //  The OpenAFile function is used for kernel open system call
+    OpenFileId OpenAFile(char *name) {
+        int fileDescriptor = OpenForReadWrite(name, FALSE);
+        if (fileDescriptor == -1) return -1;
+
+        for (int i=0; i<20; i++) {
+            if (OpenFileTable[i] == NULL) {
+                OpenFileTable[i] = new OpenFile(fileDescriptor);
+                return i+1;
+            }
+        }
+        return -1;
+    }
+    int Write(char *buffer, int size, OpenFileId id){
+         if(id > 20 or id < 1) return -1;
+        OpenFile *opFile = OpenFileTable[id-1];
+        if (opFile == NULL) return -1;
+
+        int numWritten = opFile->Write(buffer, size);
+        return numWritten;
+    }
+    int Read(char *buffer, int size, OpenFileId id){
+        if(id > 20 or id < 1) return -1;
+        OpenFile *opFile = OpenFileTable[id-1];
+        if (opFile == NULL) return -1;
+
+        int numRead = opFile->Read(buffer, size);
+        return numRead;
+    }
+    int Close(OpenFileId id){
+        if(id > 20 or id < 1) return -1;
+        OpenFile *opFile = OpenFileTable[id-1];
+        if (opFile == NULL) return -1;
+
+        delete opFile;
+        OpenFileTable[id-1] = NULL;
+        return 1;
+    }
+
     bool Remove(char *name) { return Unlink(name) == 0; }
 
 	OpenFile *fileDescriptorTable[20];
