7d2d987372c005d20ae6ad61a549cac2bb3efed1
311512023 <xup6rup4cl62333@gmail.com>
2024-01-11 14:33:48 +0800

    test

--- code/test/os_students.sh
+++ code/test/os_students.sh
@@ -1,4 +1,4 @@
 make distclean
 make 
-timeout 1 ../build.linux/nachos  -ep consoleIO_test1 10 -ep consoleIO_test2 20
+timeout 1 ../build.linux/nachos  -ep consoleIO_test1 120 -ep consoleIO_test2 130
 echo "done"
