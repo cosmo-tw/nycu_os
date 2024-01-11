a0d3d1117827066bc4c958263209934591264603
311512023 <xup6rup4cl62333@gmail.com>
2024-01-11 11:13:37 +0800

    240111-1113

--- code/filesys/filesys.h
+++ code/filesys/filesys.h
@@ -40,6 +40,8 @@
 #ifdef FILESYS_STUB 		// Temporarily implement file system calls as 
 				// calls to UNIX, until the real file system
 				// implementation is available
+typedef int OpenFileId;
+
 class FileSystem {
   public:
     FileSystem() { for (int i = 0; i < 20; i++) fileDescriptorTable[i] = NULL; }
