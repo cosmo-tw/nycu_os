6d19c79bc38291a19aebe0598e65f10692833d2d
311512023 <xup6rup4cl62333@gmail.com>
2024-01-12 14:19:39 +0800

    test

--- code/threads/scheduler.h
+++ code/threads/scheduler.h
@@ -45,7 +45,7 @@ class Scheduler {
     // SelfTest for scheduler is implemented in class Thread
     
   private:
-    // List<Thread *> *readyList;     // queue of threads that are ready to run, but not running
+    List<Thread *> *readyList;     // queue of threads that are ready to run, but not running
     Thread *toBeDestroyed;	       // finishing thread to be destroyed
     				                       // by the next thread that runs
 
